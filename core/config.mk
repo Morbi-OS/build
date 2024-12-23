44adec8eb5001e9e5a257e635d9c19642c6fa9c7
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-24 01:11:19 +0800

    build: Move inclusion of BoardConfigExtra.mk to board_config.mk
    Some kind of extras needs to alter variables that becomes read-only
    after inclusion of board_config.mk, or wants some variables getting
    processed by board_config.mk. For example, native bridge.
    
    Change-Id: If7ef0dcfc30b52940017662fa7402e4970dbc3c7
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -458,7 +458,6 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
--include vendor/extra/BoardConfigExtra.mk
 ifneq ($(AFTERLIFE_BUILD),)
 include vendor/afterlife/config/BoardConfigafterlife.mk
 endif
