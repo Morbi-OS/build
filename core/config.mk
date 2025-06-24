0acc35e7a8ae6bded25ffdc736a3606241e8d48f
texascake <tiktodz@soulvibe-team.cloud>
2025-06-24 06:41:42 +0000

    config: Migrate to Afterlife
    Change-Id: I9a65b26c090152b02c560870554b2763d86e6fd8
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- core/config.mk
+++ core/config.mk
@@ -458,8 +458,8 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-ifneq ($(INFINITY_BUILD),)
-include vendor/infinity/config/BoardConfigInfinity.mk
+ifneq ($(AFTERLIFE_BUILD),)
+include vendor/afterlife/config/BoardConfigAfterlife.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
@@ -1305,11 +1305,11 @@ include $(BUILD_SYSTEM)/sysprop_config.mk
 # consistency with those defined in BoardConfig.mk files.
 include $(BUILD_SYSTEM)/android_soong_config_vars.mk
 
-ifneq ($(INFINITY_BUILD),)
-ifneq ($(wildcard device/lineage/sepolicy/common/sepolicy.mk),)
+ifneq ($(AFTERLIFE_BUILD),)
+ifneq ($(wildcard device/afterlife/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include device/lineage/sepolicy/common/sepolicy.mk)
+$(eval include device/afterlife/sepolicy/common/sepolicy.mk)
 endif
 endif
 
